5a03bf395da97d134f507c2289dc6f5a30db1c9a
tr1ck <meyers.ricky@gmail.com>
2016-05-02 18:59:37 +0000

    Finished basics to video uploading

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -8,7 +8,7 @@ class Instructor::LessonsController < ApplicationController
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
-    redirect_to instructor_course_path(current_section)
+    redirect_to instructor_course_path(current_section.course)
   end
 
   private
@@ -25,6 +25,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle)
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 end
