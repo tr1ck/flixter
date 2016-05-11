a218f8313c8696ce5c0bf98c6c6896549ed298e9
tr1ck <meyers.ricky@gmail.com>
2016-05-11 03:48:27 +0000

    Added new lessons modal

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Flixter::Application.routes.draw do
   namespace :instructor do
     resources :lessons, only: [:update]
     resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:create]
     end
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do 
+      resources :sections, only: [:create]
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
